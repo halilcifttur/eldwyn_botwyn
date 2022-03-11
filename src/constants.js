0be8e6048eb29ecf20929a66954c987bdcdfe03a
Halil Çifttur <halil.cifttur@eplatform.com.tr>
2022-03-11 03:02:32 +0300

    whitelist updated

--- src/constants.js
+++ src/constants.js
@@ -3,5 +3,7 @@ export const OAUTH_TOKEN = 'oauth:sdacdjnx2k97dbs7a4q6lghm4dgiqz';
 export const BOT_USERNAME = 'Botwyn';
 export const ALLOWED_USERS = [
   'elongef',
-  'crystaldwarf'
+  'SiIverSurferr',
+  'Gwann',
+  'Eldwyn'
 ]
