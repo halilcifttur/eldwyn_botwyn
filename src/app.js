e5f742b0f62b17697e12b7df5389b1a27487e6a9
Halil Çifttur <halil.cifttur@eplatform.com.tr>
2022-03-11 04:36:57 +0300

    v1.0.2 update

--- src/app.js
+++ src/app.js
@@ -1,18 +1,18 @@
 import fs from 'fs'
 import tmi from 'tmi.js'
-import { BOT_USERNAME , OAUTH_TOKEN , CHANNEL_NAME , ALLOWED_USERS} from './constants';
+import { BOT_USERNAME, OAUTH_TOKEN, CHANNEL_NAME } from './constants';
 
 const options = {
   options: { debug: true, messagesLogLevel: "info" },
-	connection: {
-		reconnect: true,
-		secure: true
-	},
-	identity: {
-		username: BOT_USERNAME,
-		password: OAUTH_TOKEN
-	},
-	channels: [ CHANNEL_NAME ]
+  connection: {
+    reconnect: true,
+    secure: true
+  },
+  identity: {
+    username: BOT_USERNAME,
+    password: OAUTH_TOKEN
+  },
+  channels: [CHANNEL_NAME]
 }
 
 const client = new tmi.Client(options);
@@ -21,32 +21,87 @@ client.connect().catch(console.error);
 
 client.on('message', (channel, tags, message, self) => {
 
-	if(self) return;
+  if (self) return;
 
-  if (message.toLowerCase() === '!ölüm') {
+  // Sayaç:
+  if (message.toLowerCase() === '!ölüm')
+  {
     fs.readFile('./src/counter.txt', 'utf-8', (err, data) => {
       if (err) {
         console.error(err);
         return;
       }
       client.say(channel, data);
+      return;
     });
   }
 
-  if (message.toLowerCase() === '!öldü' && ALLOWED_USERS.some(allowedUser => tags.username.includes(allowedUser.toLowerCase()))) {
-    fs.readFile('./src/counter.txt', 'utf-8', (err, data) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      const counter = parseInt(data) + 1;
-      fs.writeFile('./src/counter.txt', `${counter}`, err => {
-        if (err) {
+  // Sayaç Artırma:
+  if (message.toLowerCase() === '!öldü')
+  {
+    const allowedUsers = fs.readFileSync('./src/whitelist.txt', 'utf-8').split(',');
+    const isWriterExist = (allowedUsers.indexOf(tags.username) > -1);
+    if (isWriterExist)
+    {
+      fs.readFile('./src/counter.txt', 'utf-8', (err, data) => {
+        if (err)
+        {
           console.error(err);
           return;
         }
-        client.say(channel, `${counter} oldu yok mu artıran KEKW`);
+
+        const counter = parseInt(data) + 1;
+        fs.writeFile('./src/counter.txt', `${counter}`, err => {
+          if (err)
+          {
+            console.error(err);
+            return;
+          }
+
+          client.say(channel, `${counter} oldu yok mu artıran KEKW`);
+          return;
+        });
       });
-    });
+    }
+  }
+
+  // Yetki:
+  if (message.toLowerCase().includes('!yetki'))
+  {
+    const user = message.substring(7);
+    const lowerCase = message.substring(7).toLowerCase();
+
+    const allowedUsers = fs.readFileSync('./src/whitelist.txt', 'utf-8').split(',');
+
+    const isWriterExist = (allowedUsers.indexOf(tags.username) > -1);
+    const isUserExist = (allowedUsers.indexOf(lowerCase) > -1);
+    if (isWriterExist)
+    {
+      if (lowerCase.length === 0)
+      {
+        client.say(channel, '!yetkiden sonra isim yazmalısın');
+        return;
+      }
+
+      if (!isUserExist)
+      {
+        allowedUsers.push(lowerCase);
+        allowedUsers.forEach(element => {
+          fs.writeFile('./src/whitelist.txt', `${allowedUsers}`, err => {
+            if (err) {
+              console.error(err);
+              return;
+            }
+          });
+        });
+        client.say(channel, `${user} yetkin verildi koçum`);
+        return;
+      }
+      else
+      {
+        client.say(channel, `${user} senin zaten yetkin var`);
+        return;
+      }
+    }
   }
 });
--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,3 @@
 export const CHANNEL_NAME = '#eldwyn';
 export const OAUTH_TOKEN = 'oauth:sdacdjnx2k97dbs7a4q6lghm4dgiqz';
 export const BOT_USERNAME = 'Botwyn';
-export const ALLOWED_USERS = [
-  'elongef',
-  'SiIverSurferr',
-  'Gwann',
-  'Eldwyn'
-]
